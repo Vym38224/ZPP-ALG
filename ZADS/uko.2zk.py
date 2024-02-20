47e2cc6f6d0cd992d8f00d5fb4d364ac98c43539
Jaroslav <jardaavymetal@gmail.com>
2024-02-20 16:25:38 +0100

    hotovo

--- ZADS/uko.2zk.py
+++ ZADS/uko.2zk.py
@@ -1,16 +1,17 @@
 def create_array(n):
     k = [{"key": None, "data": None} for x in range(n)]
-    s = {"keys": k,"top":0}
+    s = {"keys": k, "top": 0}
     return s
 
 def insert(array, key, data):
     if array["top"] < len(array["keys"]):
         array["keys"][array["top"]] = {"key": key, "data": data}
         array["top"] += 1
+    return array  
 
-f = create_array(10)
-insert(f,371,"Bitva u Leuktry, thébská hegemonie")
-insert(f,362,"Athény se Spartou poráží Théby u Mantineie")
+f = create_array(40)
+insert(f,371, "Bitva u Leuktry, thébská hegemonie")
+insert(f,362, "Athény se Spartou poráží Théby u Mantineie")
 insert(f,359,"Filippos II. zvolen králem Makedonie")
 insert(f,347,"smrt Platóna")
 insert(f,338,"bitva u Chairóneie, Řecko sjednoceno pod nadvládou Makedonců")
@@ -35,31 +36,30 @@ insert(f,202,"druhá makedonská válka, Filippos poražen a ztrácí Řecko")
 insert(f,168,"bitva u Pydny")
 insert(f,147,"achajská válka, po porážce Řeků ničí Římané Korint a zřizují provincie")
 
+
 def key_sort(i):
     return i["key"]
-l=array_set["keys"][:array_set["top"]]
-l.sort(key=key_sort)
-array_set["keys"][:array_set["top"]]=l
 
+l = f['keys'][:f['top']]
+l.sort(key=key_sort)
+f['keys'][:f['top']] = l
 
 def binary_search(array, key):
-    # Filtrujeme prvky s None klíči
-    filtered_array = [item for item in array if item['key'] is not None]
+    filtered_array = [item for item in array if item["key"] is not None]# Filtrujeme prvky s None klíči
     low = 0
     high = len(filtered_array) - 1
     while low <= high:
         mid = (low + high) // 2
-        mid_key = filtered_array[mid]['key']
+        mid_key = filtered_array[mid]["key"]
         if mid_key == key:
-            return filtered_array[mid]['data']  # Vracíme popis události
+            return filtered_array[mid]["data"]  
         elif mid_key < key:
             low = mid + 1  
         else:
             high = mid - 1  
-    return "Tento rok se nic nestalo"  # Klíč nenalezen
+    return "Tento rok se nic nestalo"  
 
-
-# Použití binárního vyhledávání na poli
+# Test
 array_set = f['keys']
-print("Rok 371:" + binary_search(array_set, 371))
-print("Rok 100:" + binary_search(array_set,100))
+print("Rok 371: " + binary_search(array_set, 371))
+print("Rok 100: " + binary_search(array_set, 100))
