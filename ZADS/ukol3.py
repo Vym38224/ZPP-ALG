b2ff72f3e72dbf41cb983a11a9e96d26f653904a
Jaroslav <jardaavymetal@gmail.com>
2024-02-26 17:28:40 +0100

    zads ukol3

---
+++ ZADS/ukol3.py
@@ -0,0 +1,25 @@
+def enqueue(Q, x):
+    if not full(Q):
+        Q["stack1"].append(x)
+
+def dequeue(Q):
+    if not empty(Q):
+        if not Q["stack2"]:
+            while Q["stack1"]:
+                Q["stack2"].append(Q["stack1"].pop())
+        return Q["stack2"].pop()
+
+def empty(Q):
+    return len(Q["stack1"]) + len(Q["stack2"]) == 0
+
+def full(Q):
+    return len(Q["stack1"]) + len(Q["stack2"]) == Q["top"]
+
+# Inicializace fronty
+Q = {"stack1": [],"stack2": [],"top": 10}
+
+# Příkazy
+for i in range(10):
+    enqueue(Q, i)
+for i in range(10):
+    print(dequeue(Q), end=",")
