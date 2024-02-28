3f8f25d138c64866d97036948a96c921d0d496be
Jaroslav <jardaavymetal@gmail.com>
2024-02-28 11:41:17 +0100

    zmena

--- ZP/seminare LS/seminar03/ukol4.py
+++ ZP/seminare LS/seminar03/ukol4.py
@@ -6,5 +6,13 @@ def aplikuj(f, sekvence):
         vysledek += f(sekvence[i],sekvence[i+1])
     return vysledek
 
-print(aplikuj(lambda x,y: x+y, [1,2,3,4]))
+print(aplikuj(lambda x,y: x+y, [1,2,3,4,5]))
 
+
+#2.verze
+def aplikuj(f, sekvence):
+    if len(sekvence) % 2 != 0:
+        sekvence.append(0)
+    return sum(f(sekvence[i], sekvence[i+1]) for i in range(0, len(sekvence), 2))
+
+print(aplikuj(lambda x, y: x + y, [1, 2, 3, 4, 5]))
