188bbce6e74ec97232176083331326a61329dcb1
Vym38224 <jardaavymetal@gmail.com>
2024-03-22 21:57:09 +0100

    zmena

--- ZP/seminare LS/seminar06_ZPU/zapoctovy_ukol1.py
+++ ZP/seminare LS/seminar06_ZPU/zapoctovy_ukol1.py
@@ -1,8 +1,6 @@
 LABEL = 0
 EDGES = 1
 
-
-
 def add_edge(graph, f, to, value):
         for node in graph:
             if node[LABEL] == f:
@@ -13,15 +11,6 @@ def add_edge(graph, f, to, value):
         v[EDGES].append((w,value))
         w[EDGES].append((v,value))
 
-"""def prvni_struktura(pocet_radku, pocet_sloupcu):
-    radky = druha_struktura_radku(0, pocet_radku)
-    sloupce = druha_struktura_sloupce(0, pocet_sloupcu)
-    root = []
-    if pocet_radku > 0:
-        add_node(root, radky)
-    if pocet_sloupcu > 0:
-        add_node(root, sloupce)
-    return root"""
 
 ############################################################¨
 def add_node(graph, label):
@@ -30,7 +19,18 @@ def add_node(graph, label):
 def vytvor_matici(pocet_radku, pocet_sloupcu):
     radky = druha_struktura_radku(0, pocet_radku)
     sloupce = druha_struktura_sloupce(0, pocet_sloupcu)
-    root = [[],pocet_radku, pocet_sloupcu]
+    root = []
+    if pocet_radku > 0:
+        add_node(root, radky)
+    if pocet_sloupcu > 0:
+        add_node(root, sloupce)
+    return root
+        
+    
+def prvni_struktura(pocet_radku, pocet_sloupcu):
+    radky = druha_struktura_radku(0, pocet_radku)
+    sloupce = druha_struktura_sloupce(0, pocet_sloupcu)
+    root = [pocet_radku, pocet_sloupcu]
     if pocet_radku > 0:
         add_node(root, radky)
     if pocet_sloupcu > 0:
@@ -49,18 +49,31 @@ def druha_struktura_sloupce(index, pocet_sloupcu):
         add_node(sloupce, druha_struktura_sloupce(index + 1, pocet_sloupcu))
     return sloupce
 
-
-def add_edge(node, edge):
-    node[1].append(edge)
+def treti_struktura(hodnota,radek,sloupec):
+    root=[hodnota,radek,sloupec]
+    return root
 
 def vloz_prvek(matice, prvek, radek, sloupec):
-    pass
+    for i in range(len(matice)):
+        for j in range(len(matice[i])):
+            if i == radek and j == sloupec:
+                matice[i][j] = treti_struktura(prvek, radek, sloupec)
                 
 
 # Testování
 matice = vytvor_matici(3, 3)
+print("Před vložením prvku:")
 print(matice)
-vloz_prvek(matice, 1, 1, 1)
+vloz_prvek(matice, 1, 0, 1)
+print("Po vložení prvku:")
+print(matice)
+
+
+
+
+
+
+
 
 
 
