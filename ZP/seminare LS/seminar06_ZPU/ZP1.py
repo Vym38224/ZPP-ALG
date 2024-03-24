81c364f24573daa94d4ec54f3f4c9baebe4908e6
Jaroslav <jardaavymetal@gmail.com>
2024-03-24 12:56:20 +0100

    zmena

--- ZP/seminare LS/seminar06_ZPU/ZP1.py
+++ ZP/seminare LS/seminar06_ZPU/ZP1.py
@@ -9,11 +9,14 @@ def vytvor_strukturu_matice(m, n):
     # Pro každý řádek vytvoříme první strukturu druhého typu a uložíme ji do seznamu radky
     for i in range(m):
         radky[i] = [i, None]  # index řádku, odkaz na první hodnotu v řádku
+        # Inicializujeme první prvek každého řádku jako None
+        radky[i][1] = None
 
     # Pro každý sloupec vytvoříme první strukturu druhého typu a uložíme ji do seznamu sloupce
     for i in range(n):
         sloupce[i] = [i, None]  # index sloupce, odkaz na první hodnotu ve sloupci
-
+        # Inicializujeme první prvek každého sloupce jako None
+        sloupce[i][1] = None
     return info_matice, radky, sloupce
 
 def vytvor_prvek(hodnota, radek, sloupec):
@@ -43,7 +46,7 @@ def pridej_prvek_na_zacatek_seznamu(seznam, novy_prvek):
 
 def vloz_prvek(matice, hodnota, radek, sloupec):
     info_matice, radky, sloupce = matice
-    m, n, _, _ = info_matice  # Rozbalení informací o matici
+    m, n, _, _ = info_matice  
 
     if radek < 0 or radek >= m or sloupec < 0 or sloupec >= n:
         print("Index řádku nebo sloupce mimo rozsah matice")
@@ -51,9 +54,8 @@ def vloz_prvek(matice, hodnota, radek, sloupec):
 
     novy_prvek = vytvor_prvek(hodnota, radek, sloupec)
 
-    # Přidání prvku do řádku a sloupce
+    # Přidání prvku pouze do seznamu řádků
     pridej_prvek_na_zacatek_seznamu(radky[radek], novy_prvek)
-    pridej_prvek_na_zacatek_seznamu(sloupce[sloupec], novy_prvek)
 
 def ziskej_hodnotu(matice, sloupec, radek):
     info_matice, radky, sloupce = matice
@@ -71,20 +73,19 @@ def ziskej_hodnotu(matice, sloupec, radek):
         aktualni_radkovy_prvek = aktualni_radkovy_prvek[3]
     return 0
 
-
-def ukaz_matici(matice):
+def zobraz_matici(matice):
     info_matice, radky, sloupce = matice
     m, n, _, _ = info_matice  
 
     for radek in range(m):
         for sloupec in range(n):
-            print(ziskej_hodnotu(matice, sloupec, radek), end=" ")
+            print(ziskej_hodnotu(matice, sloupec, radek), end="    ")
         print()
 
 # Příklad použití:
-matice = vytvor_strukturu_matice(3, 3)
-vloz_prvek(matice, 1, 0, 0)
-vloz_prvek(matice, 1, 1, 1)
-vloz_prvek(matice, 1, 2, 2)
-ukaz_matici(matice)
+matice = vytvor_strukturu_matice(3, 4)
+vloz_prvek(matice, 1, 0, 1)
+vloz_prvek(matice, 18, 0, 2)
+vloz_prvek(matice, 3, 2, 2)
+zobraz_matici(matice)
 
