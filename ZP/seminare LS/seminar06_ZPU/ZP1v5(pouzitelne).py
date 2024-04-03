94d40edb13e91bc5028f118bd536bad7ef029232
Jaroslav <jardaavymetal@gmail.com>
2024-04-03 08:28:54 +0200

    zeman

--- ZP/seminare LS/seminar06_ZPU/ZP1v5(pouzitelne).py
+++ ZP/seminare LS/seminar06_ZPU/ZP1v5(pouzitelne).py
@@ -9,29 +9,33 @@ def vloz_prvek(matice, hodnota, x, y):
     novy_prvek = [hodnota, x, y, None, None]
 
     # Vložení prvku do řádku
-    ukazatel_radku = matice[2][x]
-    if not ukazatel_radku[1] or y < ukazatel_radku[1][2]:
-        novy_prvek[3] = ukazatel_radku[1]
-        ukazatel_radku[1] = novy_prvek
-    else:
-        ukazatel_sloupce = ukazatel_radku[1]
-        while ukazatel_sloupce[3] and y > ukazatel_sloupce[3][2]:
-            ukazatel_sloupce = ukazatel_sloupce[3]
-        novy_prvek[3] = ukazatel_sloupce[3]
-        ukazatel_sloupce[3] = novy_prvek
+    if  x < matice[0] and x >= 0 and y < matice[1] and y >= 0:
+        ukazatel_radku = matice[2][x]
+        if not ukazatel_radku[1] or y < ukazatel_radku[1][2]:
+            novy_prvek[3] = ukazatel_radku[1]
+            ukazatel_radku[1] = novy_prvek
+        else:
+            ukazatel_sloupce = ukazatel_radku[1]
+            while ukazatel_sloupce[3] and y > ukazatel_sloupce[3][2]:
+                ukazatel_sloupce = ukazatel_sloupce[3]
+            novy_prvek[3] = ukazatel_sloupce[3]
+            ukazatel_sloupce[3] = novy_prvek
 
-    # Vložení prvku do sloupce
-    ukazatel_sloupce = matice[3][y]
-    if not ukazatel_sloupce[1] or x < ukazatel_sloupce[1][1]:
-        novy_prvek[4] = ukazatel_sloupce[1]
-        ukazatel_sloupce[1] = novy_prvek
+        # Vložení prvku do sloupce
+        ukazatel_sloupce = matice[3][y]
+        if not ukazatel_sloupce[1] or x < ukazatel_sloupce[1][1]:
+            novy_prvek[4] = ukazatel_sloupce[1]
+            ukazatel_sloupce[1] = novy_prvek
+        else:
+            ukazatel_radek = ukazatel_sloupce[1]
+            while ukazatel_radek[4] and x > ukazatel_radek[4][1]:
+                ukazatel_radek = ukazatel_radek[4]
+            novy_prvek[4] = ukazatel_radek[4]
+            ukazatel_radek[4] = novy_prvek
     else:
-        ukazatel_radek = ukazatel_sloupce[1]
-        while ukazatel_radek[4] and x > ukazatel_radek[4][1]:
-            ukazatel_radek = ukazatel_radek[4]
-        novy_prvek[4] = ukazatel_radek[4]
-        ukazatel_radek[4] = novy_prvek
-
+        print(f"Pozor na správný počet řádku nebo sloupců matice! Prvek s hodnotou {hodnota} nepřidán do naší matice typu {matice[0]}×{matice[1]}")
+        print()
+        
 
 def zobraz_matici(matice):
     ukazatel_radku = matice[2]
@@ -48,11 +52,13 @@ def zobraz_matici(matice):
         print()
 
 # Test
-matice = vytvor_matici(3, 3)
-vloz_prvek(matice, 1, 0, 1)
+matice = vytvor_matici(3, 4)
+vloz_prvek(matice, 1, 0, 0)
 vloz_prvek(matice, 2, 1, 1)
 vloz_prvek(matice, 3, 1, 2)
 vloz_prvek(matice, 4, 2, 2)
+vloz_prvek(matice, 5, -2, 0)
+vloz_prvek(matice, 6, 3, 0)
 zobraz_matici(matice)
 print()
 print(matice)
