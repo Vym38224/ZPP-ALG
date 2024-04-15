bb8b5fb1b396494a0caf857fa498dec20eedba75
Vym38224 <jardaavymetal@gmail.com>
2024-04-15 17:54:54 +0200

    zeman

--- ZP/seminare LS/seminar09/ukol4.py
+++ ZP/seminare LS/seminar09/ukol4.py
@@ -18,22 +18,14 @@ def convert_number_to_bytes(number):
         
 
 
-bytes_array = convert_number_to_bytes(9308)
-number = convert_bytes_to_number(list(bytes_array))
+#bytes_array = convert_number_to_bytes(9308)
+#number = convert_bytes_to_number(list(bytes_array))
 
-#number = convert_bytes_to_number([2,0])
-#bytes_array = convert_number_to_bytes(number)
 
-# Použití bytes
-try:
-    f = open("ZP/seminare LS/seminar09/soubor.bin", "wb")
-    f.write(bytes(bytes_array))
-    print("Bytes:", bytes_array)
-except Exception as e:
-    # ošetření výjimek
-    print("Chyba při práci se souborem bytes:", e)
-finally:
-    f.close()
+number = convert_bytes_to_number([2,0])
+bytes_array = convert_number_to_bytes(number)
+
+
 
 # Použití number
 try:
@@ -45,3 +37,15 @@ except Exception as e:
     print("Chyba při práci se souborem number:", e)
 finally:
     f1.close()
+
+
+# Použití bytes_array
+try:
+    f = open("ZP/seminare LS/seminar09/soubor.bin", "wb")
+    f.write(bytes(bytes_array))
+    print("Bytes:", bytes_array)
+except Exception as e:
+    # ošetření výjimek
+    print("Chyba při práci se souborem bytes:", e)
+finally:
+    f.close()
