5f7eb33a15fd1785e4c537c10637b4e4e1b9805e
Jaroslav <jardaavymetal@gmail.com>
2024-04-16 09:42:02 +0200

    xml zacatek

--- UDITE/pacienti.sql
+++ UDITE/pacienti.sql
@@ -1,23 +1,24 @@
 CREATE TABLE Pacienti (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    krestni_jmeno VARCHAR(50),
-    prijmeni VARCHAR(50),
-    rodne_cislo VARCHAR(11) UNIQUE,
+    krestni_jmeno VARCHAR(50) NOT NULL,
+    prijmeni VARCHAR(50) NOT NULL,
+    rodne_cislo VARCHAR(11) PRIMARY KEY,
     email VARCHAR(100) DEFAULT 'Není registrován',
-    telefon VARCHAR(20) DEFAULT '',
-    registrovany VARCHAR(3) DEFAULT 'ne' CHECK (registrovany IN ('ano', 'ne')),
-    datum_registrace DEFAULT ''
+    telefon INT,
+    registrovany VARCHAR(3) DEFAULT ne,
+    datum_registrace DATE,
+    CHECK(registrovany in ('ano', 'ne'))
 );
 
 -- Vložení záznamů
 INSERT INTO Pacienti (krestni_jmeno, prijmeni, rodne_cislo, email, telefon, registrovany, datum_registrace) VALUES
-('Tomáš', 'Marný', '800101/1234', 'tomas@marny.cz', '777545890', DEFAULT, '2020-03-28'),
-('Marek', 'Procházka', '851026/1234', 'marek@prochazka.cz', '604585375', DEFAULT, '2017-07-19'),
-('Tomáš', 'Pospíšil', '935511/6789', DEFAULT, DEFAULT, DEFAULT, DEFAULT),
-('Adéla', 'Nováková', '840212/9876', 'adela@novakova.cz', '585321670', DEFAULT, '2019-01-07'),
-('Matyáš', 'Havelka', '970922/5544', DEFAULT, DEFAULT, DEFAULT, DEFAULT),
-('Barbora', 'Navrátilová', '861201/0099', 'barbora@navratilova.cz', '602555999', DEFAULT, '2018-12-13'),
-('Veronika', 'Vlková', '800101/1234', 'veronika@vlkova.cz', '908987654', DEFAULT, '2019-01-07');
+('Tomáš', 'Marný', '800101/1234', 'tomas@marny.cz', '777545890', 'ano', '2020-03-28'),
+('Marek', 'Procházka', '851026/1234', 'marek@prochazka.cz', '604585375','ano', '2017-07-19'),
+('Tomáš', 'Pospíšil', '935511/6789','Není registrován','','ne',''),
+('Adéla', 'Nováková', '840212/9876', 'adela@novakova.cz', '585321670','ano', '2019-01-07'),
+('Matyáš', 'Havelka', '970922/5544','Není registrován','','ne',''),
+('Barbora', 'Navrátilová', '861201/0099', 'barbora@navratilova.cz', '602555999','ano', '2018-12-13'),
+('Veronika', 'Vlková', '800102/1234', 'veronika@vlkova.cz', '908987654','ano', '2019-01-07');
+
 
 
 -- A: Zobrazte všechna křestní jména tak, aby se neopakovala.
