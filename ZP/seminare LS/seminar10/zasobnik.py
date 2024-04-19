c0c37b0abf56f15b376a80d9d8a710974379335e
Vym38224 <jardaavymetal@gmail.com>
2024-04-19 14:56:04 +0200

    zacatek zpu2

--- ZP/seminare LS/seminar10/zasobnik.py
+++ ZP/seminare LS/seminar10/zasobnik.py
@@ -1,11 +1,11 @@
 def vytvor_zasobnik():
     return []
 
-def pridej_do_zasobniku(zasobnik,hodnota):
+def pridej_do_zasobniku(zasobnik, hodnota):
     zasobnik.append(hodnota)
 
 def odeber_ze_zasobniku(zasobnik):
     if zasobnik:
-        zasobnik.pop()
-    else:
-        pass
+         return zasobnik.pop()
+
+
--- ZP/seminare LS/seminar11_ZPU2/test.py
+++ ZP/seminare LS/seminar11_ZPU2/test.py
@@ -1,10 +1,41 @@
-A = {1,2,7}
-B = {5,6,7}
-C = {0,1,2,3,4,5,6,7}
-D = {}
-E = {2,4,8}
+import bit_set
 
-set_a = 134
-set_b = 224
-intersection = set_a & set_b
+mnozina = bit_set.create_bit_set(10)
+print(mnozina)  # Output: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+mnozina_s_hodnotou = bit_set.create_bitset_with_values(10, [2, 5, 7])
+print(mnozina_s_hodnotou)  # Output: [0, 0, 1, 0, 0, 1, 0, 1, 0, 0]
+
+mnozina_upto = bit_set.create_bitset_with_range(10, 8)
+print(mnozina_upto)  # Output: [1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
+
+pridej = bit_set.insert(mnozina, 3)  # změní index 3 hodnotu 1
+print(mnozina)  
+
+odeber = bit_set.remove(mnozina, 3)  # změní index 3 hodnotu 0
+print(mnozina)  
+
+pridej = bit_set.insert(mnozina, 3)  # změní index 3 hodnotu 1
+print(mnozina)  
+
+zkontroluj = bit_set.contains(mnozina, 3)  # zkontroluje zda je na indexu 3 1/0 (True/False)
+print(zkontroluj)  
+
+left_set = [1, 0, 1, 1]
+right_set = [1, 0, 1, 1, 0]
+
+print(bit_set.intersection(left_set, right_set))  # průnik
+print(bit_set.union(left_set, right_set))         # sjednocení
+print(bit_set.difference(left_set, right_set))    # rozdíl pokud size(left) >= size(right)
+print(bit_set.is_subset(left_set, right_set))     # být podmnožinou (left podmnožinou right)
+
+A = [[1, 3, 5, 7, 9, 20],[4,5,7]]
+
+bit_set.save_bitsets_to_file("ZP/seminare LS/seminar11_ZPU2/bit_sets.txt", A)
+
+loaded_bitsets = bit_set.load_bitsets("ZP/seminare LS/seminar11_ZPU2/bit_sets.txt")
+print(loaded_bitsets)
+
+condition =  lambda bitset: max(bitset) <= 10 
+filtered_bitsets = bit_set.load_bitsets_if(condition,"ZP/seminare LS/seminar11_ZPU2/bit_sets.txt")
+print(filtered_bitsets)
