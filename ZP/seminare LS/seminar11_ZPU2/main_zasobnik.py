e8dd543534bec3d5789011f6867c78b957ca2a8a
Jaroslav <jardaavymetal@gmail.com>
2024-04-24 22:44:50 +0200

    zacatek ukolu 2

--- ZP/seminare LS/seminar11_ZPU2/main_zasobnik.py
+++ ZP/seminare LS/seminar11_ZPU2/main_zasobnik.py
@@ -1,6 +1,6 @@
 import zasobnik
 
-z1 = zasobnik.vytvor_zasobnik()
+z1 = zasobnik.vytvor_zasobnik(2)
 zasobnik.pridej_do_zasobniku(z1, 16)
 zasobnik.pridej_do_zasobniku(z1, 65536)
 zasobnik.pridej_do_zasobniku(z1, 65535)
@@ -8,6 +8,8 @@ zasobnik.pridej_do_zasobniku(z1, 3)
 zasobnik.pridej_do_zasobniku(z1, -3)
 
 
+"""zasobnik.zapis_do_souboru("zasobnik.bin", z1)"""
+z2 = zasobnik.nacti_ze_souboru("zasobnik.bin")
 print(zasobnik.odeber_ze_zasobniku(z1))
 print(zasobnik.odeber_ze_zasobniku(z1))
 print(zasobnik.odeber_ze_zasobniku(z1))
--- ZP/seminare LS/seminar11_ZPU2/zasobnik.py
+++ ZP/seminare LS/seminar11_ZPU2/zasobnik.py
@@ -1,36 +1,62 @@
+
 PRVNI_PRVEK = 0
 DALSI_PRVEK = 1
 
-def vytvor_zasobnik():
-    Zasobnik = []
-    return Zasobnik
+def convert_number_to_bytes(number):
+    bytes_array = []
+    if number:
+        while number:
+            byte = number & 0xff
+            bytes_array.append(byte)
+            number = number >> 8
+    else:
+        bytes_array.append(0)
+    return bytes_array[::-1]
+
+def vytvor_zasobnik(max_bajty):
+    return max_bajty, []
 
 def pridej_do_zasobniku(seznam, x):
-    while seznam:
-        seznam = seznam[DALSI_PRVEK]
-    seznam.extend([x, []])
+    max_bajty, zasobnik = seznam
+    if 0 < x < 2**(max_bajty * 8):
+        while zasobnik:
+            zasobnik = zasobnik[DALSI_PRVEK]
+        zasobnik.extend([x, []])
+    else:
+        print(f"hodnota {x} je mimo povolený rozsah.")
 
+def zapis_do_souboru(nazev_souboru, zasobnik):
+    max_bajty, data = zasobnik
+    with open(nazev_souboru, "wb") as file:
+        file.write(bytes([max_bajty]))
+        file.write(bytes(data))
+
+def nacti_ze_souboru(nazev_souboru):
+    with open(nazev_souboru, "rb") as file:
+        max_bajty = int.from_bytes(file.read(1), byteorder='big')
+        zasobnik = list(file.read())
+    return max_bajty, zasobnik
 
 def odeber_ze_zasobniku(seznam):
-    if not seznam:
+    max_bajty, zasobnik = seznam
+    if not zasobnik:
         return None
     
-    predchozi = None
-    aktualni = seznam
+    predchozi = []
+    aktualni = zasobnik
 
-    while aktualni is not None and aktualni[DALSI_PRVEK]:
+    while aktualni and aktualni[DALSI_PRVEK]:
         predchozi = aktualni
         aktualni = aktualni[DALSI_PRVEK]
     
-    if predchozi is None:
-        odebrany_prvek = seznam[PRVNI_PRVEK]
-        seznam[PRVNI_PRVEK] = None  
+    if not predchozi :
+        odebrany_prvek = zasobnik[PRVNI_PRVEK]
+        zasobnik[:] = [] 
     else:
         odebrany_prvek = predchozi[DALSI_PRVEK][PRVNI_PRVEK]
-        predchozi[DALSI_PRVEK] = None  
+        predchozi[DALSI_PRVEK] = []  
 
     return odebrany_prvek
-
 
 
 
